4edcba586159c4420545b729a90c2d78cca3e94a
Daniel <dannelinder21@gmail.com>
2017-10-01 07:29:19 +0200

    Add project files.

---
+++ Delegates_ShoeExample/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_ShoeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fills a list with some shoes
+            List<Shoe> shoes = new List<Shoe>();
+
+            shoes.Add(new Shoe("Brown", 38, "Nike"));
+            shoes.Add(new Shoe("Black", 41, "Adidas"));
+            shoes.Add(new Shoe("Black", 35, "Puma"));
+            shoes.Add(new Shoe("White", 40, "Nike"));
+            shoes.Add(new Shoe("Black", 45, "Nike"));
+            shoes.Add(new Shoe("White", 42, "Puma"));
+            shoes.Add(new Shoe("Brown", 39, "Puma"));
+            shoes.Add(new Shoe("Black", 39, "Nike"));
+            shoes.Add(new Shoe("Brown", 44, "Adidas"));
+            shoes.Add(new Shoe("Black", 46, "Nike"));
+
+            // Using Lambda to find all brown shoes
+            Console.WriteLine("All brown shoes:");
+            foreach (var shoe in shoes.FindAll(c => c.Color == "Brown"))
+            {
+                Console.WriteLine($"{ shoe.Brand}, { shoe.Size}");
+            }
+            Console.WriteLine("-------------------------------------------");
+
+            // Calls method 'FindShoeBySize'
+            Console.WriteLine("All shoes bigger than size 43:");
+            foreach (var shoe in shoes.FindAll(FindShoeBySize))
+            {
+                Console.WriteLine($"{ shoe.Brand}, { shoe.Size}");
+            }
+            Console.WriteLine("-------------------------------------------");
+
+        }
+
+        // Named method that returns true if more than size 43
+        static bool FindShoeBySize(Shoe thisShoe)
+        {
+            return thisShoe.Size > 43;
+        }
+    }
+}
---
+++ Delegates_ShoeExample/Shoe.cs
@@ -0,0 +1,17 @@
+﻿namespace Delegates_ShoeExample
+{
+    public class Shoe
+    {
+        public string Color { get; set; }
+        public int Size { get; set; }
+        public string Brand { get; set; }
+
+        public Shoe(string color, int size, string brand)
+        {
+            Color = color;
+            Size = size;
+            Brand = brand;
+        }
+    }
+
+}
